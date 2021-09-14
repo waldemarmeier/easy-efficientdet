d477da802d3e0ca29c8c61ad1690d5543c4bffcb
waldemarmeier <waldemar_meier@yahoo.de>
2021-09-15 00:26:49 +0200

    added config files

--- setup.py
+++ setup.py
@@ -2,3 +2,15 @@
 
 # integrate scripts with entry point
 # https://stackoverflow.com/questions/774824/explain-python-entry-points
+
+from distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1dev',
+    packages=[
+        'towelstuff',
+    ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
