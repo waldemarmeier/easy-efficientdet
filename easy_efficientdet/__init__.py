9e706f2ef39886d505dc6d58f1fc650e099ca1d7
waldemarmeier <waldemar_meier@yahoo.de>
2021-09-19 17:47:01 +0200

    add missing __init__.py files

--- easy_efficientdet/__init__.py
+++ easy_efficientdet/__init__.py
@@ -1,3 +1,6 @@
 from . import visualization
+from .config import DefaultConfig
+from .data.preprocessing import init_data
 from .losses import ObjectDetectionLoss
 from .model import EfficientDet
+from .training import CosineLrSchedule
