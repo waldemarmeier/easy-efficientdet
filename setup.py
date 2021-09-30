afe0b18399ab4e49d7b9e49922fca036463b4f16
waldemarmeier <waldemar_meier@yahoo.de>
2021-10-01 00:06:40 +0200

    more work on packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
-# TODO major todo
-# integrate scripts with entry point
-# https://stackoverflow.com/questions/774824/explain-python-entry-points
-
-requirements = {"default": ["tensorflow>=2.3.0"], "optional": {"": ""}}
+requirements = {
+    "default": ["tensorflow>=2.3.0"],
+    "extra": {
+        "eval": ["pycocotools>=2.0.2"]
+    }
+}
 
 
 def load_readme() -> str:
@@ -12,21 +13,35 @@ def load_readme() -> str:
         return fp.read()
 
 
-setup(name='easy-efficientdet',
-      version='0.1',
-      author="Waldemar Meier",
-      author_email="info@waldemarmeier.com",
-      url="https://github.com/waldemarmeier/easy-efficientdet",
-      keywords="object-detection neural-network efficientdet",
-      packages=find_packages(exclude=["easy_efficientdet._third_party*"]),
-      license='Apache 2.0',
-      description="Easy to use object detection package based on tensorflow",
-      long_description=load_readme(),
-      entry_points={
-          'console_scripts': [
-              'generate_tfrecord = easy_efficientdet.tools.generate_tfrecord:main',
-              'create_labelmap = easy_efficientdet.tools.create_labelmap:main',
-              'prepare_voc = '
-              'easy_efficientdet.tools.create_object_detection_dataset:main',
-          ],
-      })
+setup(
+    name='easy-efficientdet',
+    version='0.1',
+    author="Waldemar Meier",
+    author_email="info@waldemarmeier.com",
+    url="https://github.com/waldemarmeier/easy-efficientdet",
+    keywords="object-detection neural-network efficientdet",
+    packages=find_packages(exclude=["easy_efficientdet._third_party*"]),
+    license='Apache 2.0',
+    python_requires='>=3.7',
+    install_requires=requirements["default"],
+    extras_require=requirements["extra"],
+    description="Easy to use object detection package based on tensorflow",
+    long_description=load_readme(),
+    entry_points={
+        'console_scripts': [
+            'generate_tfrecord = easy_efficientdet.tools.generate_tfrecord:main',
+            'create_labelmap = easy_efficientdet.tools.create_labelmap:main',
+            'prepare_voc = '
+            'easy_efficientdet.tools.create_object_detection_dataset:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
