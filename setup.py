a83f464b560c10ca1456d3b731b6a4a3bf2bb03b
waldemarmeier <waldemar_meier@yahoo.de>
2021-09-30 23:51:31 +0200

    setup work in progress

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
-# TODO major todo
+from setuptools import find_packages, setup
 
+# TODO major todo
 # integrate scripts with entry point
 # https://stackoverflow.com/questions/774824/explain-python-entry-points
 
-from distutils.core import setup
+requirements = {"default": ["tensorflow>=2.3.0"], "optional": {"": ""}}
 
-setup(
-    name='TowelStuff',
-    version='0.1dev',
-    packages=[
-        'towelstuff',
-    ],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
+
+def load_readme() -> str:
+    with open('README.md') as fp:
+        return fp.read()
+
+
+setup(name='easy-efficientdet',
+      version='0.1',
+      author="Waldemar Meier",
+      author_email="info@waldemarmeier.com",
+      url="https://github.com/waldemarmeier/easy-efficientdet",
+      keywords="object-detection neural-network efficientdet",
+      packages=find_packages(exclude=["easy_efficientdet._third_party*"]),
+      license='Apache 2.0',
+      description="Easy to use object detection package based on tensorflow",
+      long_description=load_readme(),
+      entry_points={
+          'console_scripts': [
+              'generate_tfrecord = easy_efficientdet.tools.generate_tfrecord:main',
+              'create_labelmap = easy_efficientdet.tools.create_labelmap:main',
+              'prepare_voc = '
+              'easy_efficientdet.tools.create_object_detection_dataset:main',
+          ],
+      })
